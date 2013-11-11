ca666f42cddf68d7f7134ade6bfb55fab3f6815c
Uchio KONDO <udzura@paperboy.co.jp>
2013-11-11 12:21:42 +0900

    Test more situations

--- test/controllers/status_engine/status_controller_test.rb
+++ test/controllers/status_engine/status_controller_test.rb
@@ -7,5 +7,29 @@ module StatusEngine
       assert_response :success
     end
 
+    test "should contain status and revision" do
+      get :index, use_route: :status_engine
+      assert_equal response_json["status"], "OK"
+      assert_equal response_json["revision"], "0000000000000000000000000000000000000000"
+    end
+
+    test "should look into revision file" do
+      require 'fakefs/safe'
+      FakeFS do
+        FileUtils.mkdir_p Rails.root
+        File.open Rails.root.join("REVISION"), "w" do |f|
+          f.puts "abcdabcdabcdabcdabcdabcdabcdabcdabcdabcd"
+        end
+
+        get :index, use_route: :status_engine
+        assert_equal response_json["status"], "OK"
+        assert_equal response_json["revision"], "abcdabcdabcdabcdabcdabcdabcdabcdabcdabcd"
+      end
+    end
+
+    private
+    def response_json
+      JSON.parse response.body
+    end
   end
 end
--- test/integration/navigation_test.rb
+++ test/integration/navigation_test.rb
@@ -3,8 +3,15 @@ require 'test_helper'
 class NavigationTest < ActionDispatch::IntegrationTest
   fixtures :all
 
-  # test "the truth" do
-  #   assert true
-  # end
-end
+  test "works well" do
+    get "/status_engine/status"
+    assert_response :success
+    assert response.body.include? "0000000000000000000000000000000000000000"
+  end
 
+  test "routed well" do
+    assert_raises(ActionController::RoutingError) do
+      get "/status_engine/status/hoge"
+    end
+  end
+end
